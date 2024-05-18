f05ff96833992378a41bde23c367c3c07dd65271
Jagapotaniki <soccer7.19.basketball@ezweb.ne.jp>
2024-05-19 03:47:37 +0900

    01Games 2nd_commit

--- zeroone.c
+++ zeroone.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void drowLine();
+
 int main(void) {
 
     int gamemode, point, maxround, score;
@@ -20,7 +22,7 @@ int main(void) {
         point = pointsettings[gamemode - 1];
         maxround = roundsettings[gamemode - 1];
 
-        printf("----------------------------------------\n");
+        drowLine();
         printf("Round | Scored | To Go \n");
         for( int round = 1; round <= maxround; round++ ) {
             printf("Round %d | %d | ", round, point);
@@ -32,16 +34,22 @@ int main(void) {
                 point += score;
             }
             if( point == 0 ) {
+                drowLine();
                 printf("Finish!!!\n");
+                drowLine();
                 break;
             } 
             if( round == maxround ) {
-                printf("--------------------\n");
+                drowLine();
                 printf("Round Over\n");
                 printf("Score : %d\n", point );
-                printf("--------------------\n");
+                drowLine();
             }
         }
     }
 
+}
+
+void drowLine() {
+    printf("--------------------------------\n");
 }
