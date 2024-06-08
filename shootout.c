9a63bb68bdd9dd60e9227805b3daa11f5031df39
Jagapotaniki <soccer7.19.basketball@ezweb.ne.jp>
2024-06-09 04:01:13 +0900

    ShootOut 10% Completed

--- shootout.c
+++ shootout.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include <stdbool.h> 
 
 int main(void) {
-    
+
+    bool target[20] = {false};
+    int multiple = 1;
+    int round, start;
+
+    while(1) {
+        printf( "1:Start, 0:Exit" );
+        scanf( "%d", &start );
+        if( start == 1 ) {
+
+        } else if( start == 0 ) {
+
+        }
+    }
 }
